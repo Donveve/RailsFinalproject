b17eef9d5674ff401a68c179ebc7c65713b55cb6
Donveve <vdorestave@gmail.com>
2019-04-03 15:05:48 -0400

    API facebook added

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,11 @@ class User < ApplicationRecord
 
   devise :omniuathable, omniauth_providers: [:facebook]
 
+def self.from_facebook(auth)
+  where(facebook_id:auth.uid).first_or_create do |user|
+    user.email = auth.info.email
+    user.password = Devise.friendly_token[0,20]
+    user.skip_confirmation!
+  end
+end
 end
